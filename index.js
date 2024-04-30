7d1b84afe5475432df5cfca2434a493562384852
unknown <sanjidaema81@gmail.com>
2024-04-30 22:40:52 +0600

    added all section

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri)
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
